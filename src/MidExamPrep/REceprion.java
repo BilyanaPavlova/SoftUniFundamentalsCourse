ae114d8013906d5b7565e8ba77a827d2cc78561c
Bilyana Aneva <bilyana.aneva@mobisystems.com>
2023-10-20 21:20:35 +0300

    2 Mid Exam: Reception, HeartsDelivery (lists)

---
+++ src/MidExamPrep/REceprion.java
@@ -0,0 +1,24 @@
+package MidExamPrep;
+
+import java.util.Scanner;
+
+public class REceprion {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int firstEmEfficiency = Integer.parseInt(sc.nextLine());
+        int secondEmEfficiency = Integer.parseInt(sc.nextLine());
+        int thirdEmEfficiency = Integer.parseInt(sc.nextLine());
+        int students = Integer.parseInt(sc.nextLine());
+        int totalEfficiencyPerHour = firstEmEfficiency + secondEmEfficiency + thirdEmEfficiency;
+        double hoursPureWork = Math.ceil(students * 1.0 / totalEfficiencyPerHour);
+
+        double breaks = Math.floor(hoursPureWork / 3);
+        double totalHours = hoursPureWork + breaks;
+
+
+        System.out.printf("Time needed: %.0fh.", (totalHours));
+    }
+
+}
